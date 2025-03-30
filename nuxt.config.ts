a402bcf7357832a54f60719cf240dd4ccfab2032
Hacker God <jyrone.parker@gmail.com>
2025-03-29 23:47:01 -0400

    pwa need to fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,28 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     baseURL: '/shake-tv-nuxt/'
+  },
+  pwa: {
+    manifest: {
+      name: 'Shake TV',
+      short_name: 'Shake TV',
+      description: 'Watch and share short videos.',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: 'https://api.shaketv.net/.*',
+          handler: 'NetworkFirst'
+        }
+      ]
+    }
   }
 })
